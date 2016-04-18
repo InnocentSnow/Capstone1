6d74daf5f14d95572d0a637e70e731a4795bf185
InnocentSnow <elwe@nate.com>
2016-04-18 17:26:29 +0900

    p1

---
+++ Capstone/P1_log/autonpm.bat
@@ -0,0 +1,8 @@
+call npm install express --save
+call npm install external-ip --save
+call npm install ip --save
+call npm install cookie-parser --save
+call npm install body-parser --save
+call npm install fs --save
+call npm install date-utils --save
+call npm install ua-parser-js --save
